1fa38ca8b0997287f4a59bf945629f85c271d2d3
Frederick Michielssen <fmichielssen@openanalytics.eu>
2016-08-17 14:59:24 +0200

    Added some extra threadsafe dialogs for use in the imageanalysis addon.

--- eu.openanalytics.phaedra.base.ui.util/src/eu/openanalytics/phaedra/base/ui/util/misc/ThreadsafeDialogHelper.java
+++ eu.openanalytics.phaedra.base.ui.util/src/eu/openanalytics/phaedra/base/ui/util/misc/ThreadsafeDialogHelper.java
@@ -2,7 +2,10 @@ package eu.openanalytics.phaedra.base.ui.util.misc;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
 
 /**
@@ -60,5 +63,33 @@ public class ThreadsafeDialogHelper {
 				MessageDialog.openInformation(Display.getDefault().getActiveShell(), title, message);
 			}
 		});
+	}
+	
+	public static String openInput(final String title, final String message, final String initValue) {
+		final StringBuilder sb = new StringBuilder();
+		Display.getDefault().syncExec(new Runnable() {
+			@Override
+			public void run() {
+				InputDialog dialog = new InputDialog(Display.getCurrent().getActiveShell(), title, message, initValue, null);
+				int retCode = dialog.open();
+				if (retCode == Window.OK) sb.append(dialog.getValue());
+			}
+		});
+		return sb.toString();
+	}
+	
+	public static String openFolderSelector(final String message, final String selectedPath) {
+		final StringBuilder sb = new StringBuilder();
+		Display.getDefault().syncExec(new Runnable() {
+			@Override
+			public void run() {
+				DirectoryDialog dialog = new DirectoryDialog(Display.getCurrent().getActiveShell());
+				dialog.setMessage(message);
+				if (selectedPath != null) dialog.setFilterPath(selectedPath);
+				String retVal = dialog.open();
+				if (retVal != null) sb.append(retVal);
+			}
+		});
+		return sb.toString();
 	}
 }
