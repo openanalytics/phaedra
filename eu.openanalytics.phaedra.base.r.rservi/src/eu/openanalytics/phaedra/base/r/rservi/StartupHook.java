9573e45c90615fe3f53a37b15241f682d0ac9373
Frederick Michielssen <fmichielssen@openanalytics.eu>
2017-08-11 15:23:37 +0200

    Made R startup async, to decrease waiting time on the splash screen

--- eu.openanalytics.phaedra.base.r.rservi/src/eu/openanalytics/phaedra/base/r/rservi/StartupHook.java
+++ eu.openanalytics.phaedra.base.r.rservi/src/eu/openanalytics/phaedra/base/r/rservi/StartupHook.java
@@ -4,26 +4,29 @@ import eu.openanalytics.phaedra.base.hook.BaseHook;
 import eu.openanalytics.phaedra.base.hook.IHookArguments;
 import eu.openanalytics.phaedra.base.ui.trafficlight.StatusManager;
 import eu.openanalytics.phaedra.base.util.misc.EclipseLog;
+import eu.openanalytics.phaedra.base.util.threading.JobUtils;
 
 public class StartupHook extends BaseHook {
 
 	@Override
 	public void post(IHookArguments args) {
-		EclipseLog.info("Starting R engine", Activator.getDefault());
-		RService.getInstance().initialize();
-		
-		try {
-			StatusManager.getInstance().forcePoll(RStatusChecker.class);
-		} catch(Exception e) {
-			// Running in headless mode.
-		}
-		
-		boolean isRunning = RService.getInstance().isRunning();
-		if (isRunning) {
-			EclipseLog.info("R engine up and running", Activator.getDefault());
-		} else {
-			EclipseLog.warn("R engine not available! Check the log file for error messages.", Activator.getDefault());
-		}
+		JobUtils.runBackgroundJob(() -> {
+			EclipseLog.info("Starting R engine", Activator.getDefault());
+			RService.getInstance().initialize();
+			
+			try {
+				StatusManager.getInstance().forcePoll(RStatusChecker.class);
+			} catch(Exception e) {
+				// Running in headless mode.
+			}
+			
+			boolean isRunning = RService.getInstance().isRunning();
+			if (isRunning) {
+				EclipseLog.info("R engine up and running", Activator.getDefault());
+			} else {
+				EclipseLog.warn("R engine not available! Check the log file for error messages.", Activator.getDefault());
+			}
+		});
 	}
 
 }
