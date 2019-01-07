c92735d5b3b14b278e8d1ff886a5449c76e6ddd9
Frederick Michielssen <fmichielssen@openanalytics.eu>
2019-01-07 13:13:30 +0100

    Image canvas renderer: switched default to incremental

--- eu.openanalytics.phaedra.ui.wellimage/src/eu/openanalytics/phaedra/ui/wellimage/canvas/CanvasRendererFactory.java
+++ eu.openanalytics.phaedra.ui.wellimage/src/eu/openanalytics/phaedra/ui/wellimage/canvas/CanvasRendererFactory.java
@@ -2,8 +2,18 @@ package eu.openanalytics.phaedra.ui.wellimage.canvas;
 
 public class CanvasRendererFactory {
 
+	private static final String PROP_RENDER_TYPE = "phaedra.canvas.renderer";
+	
+	private static final String RENDER_TYPE_INCR = "incremental";
+	private static final String RENDER_TYPE_SLICED = "sliced";
+	
 	public static ICanvasRenderer createRenderer() {
-		if ("incremental".equalsIgnoreCase(System.getProperty("phaedra.canvas.renderer"))) return new IncrementalCanvasRenderer();
-		else return new SlicedCanvasRenderer();
+		String renderType = System.getProperty(PROP_RENDER_TYPE);
+		if (renderType == null || renderType.isEmpty() || renderType.toLowerCase().equals(RENDER_TYPE_INCR)) {
+			return new IncrementalCanvasRenderer();
+		} else if (renderType.toLowerCase().equals(RENDER_TYPE_SLICED)) {
+			return new SlicedCanvasRenderer();
+		}
+		throw new IllegalArgumentException("Invalid setting: " + PROP_RENDER_TYPE + "=" + renderType);
 	}
 }
