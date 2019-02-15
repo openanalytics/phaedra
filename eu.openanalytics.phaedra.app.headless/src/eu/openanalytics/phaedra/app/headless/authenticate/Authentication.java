ae3e27550ce317afcf2a3daa9cdbba101cefb156
Frederick Michielssen <fmichielssen@openanalytics.eu>
2019-02-15 14:57:33 -0500

    Added option for headless auth to use pwd id instead of password

--- eu.openanalytics.phaedra.app.headless/src/eu/openanalytics/phaedra/app/headless/authenticate/Authentication.java
+++ eu.openanalytics.phaedra.app.headless/src/eu/openanalytics/phaedra/app/headless/authenticate/Authentication.java
@@ -7,6 +7,7 @@ import eu.openanalytics.phaedra.app.headless.Activator;
 import eu.openanalytics.phaedra.base.environment.EnvironmentRegistry;
 import eu.openanalytics.phaedra.base.environment.IEnvironment;
 import eu.openanalytics.phaedra.base.environment.Screening;
+import eu.openanalytics.phaedra.base.environment.config.ConfigLoader;
 import eu.openanalytics.phaedra.base.security.AuthenticationException;
 import eu.openanalytics.phaedra.base.security.PermissionDeniedException;
 import eu.openanalytics.phaedra.base.util.misc.EclipseLog;
@@ -16,12 +17,22 @@ public class Authentication {
 	private static final String PROP_ENV = "phaedra.env";
 	private static final String PROP_USER = "phaedra.username";
 	private static final String PROP_PW = "phaedra.password";
-
+	private static final String PROP_PW_ID = "phaedra.pwd-id";
+	
 	public static boolean authenticate() throws PermissionDeniedException {
 		Properties config = Activator.getDefault().getHeadlessProperties();
 	
 		String username = config.getProperty(PROP_USER);
 		String password = config.getProperty(PROP_PW);
+		String pwdId = config.getProperty(PROP_PW_ID);
+		
+		if (pwdId != null && !pwdId.isEmpty()) {
+			try {
+				password = ConfigLoader.loadConfig().resolvePassword(pwdId);
+			} catch (Exception e) {
+				throw new IllegalArgumentException(String.format("Failed to retrieve password with id %s", pwdId));
+			}
+		}
 		
 		if (username == null || password == null) {
 			throw new PermissionDeniedException(String.format("Cannot authenticate: properties %s and %s not set", PROP_USER, PROP_PW));
