e9a562a773492db01b315389036164497099114c
Frederick Michielssen <fmichielssen@openanalytics.eu>
2019-03-06 11:45:48 +0100

    Made the db pool size setting accessible to users

--- eu.openanalytics.phaedra.base.db/src/eu/openanalytics/phaedra/base/db/prefs/PreferencePage.java
+++ eu.openanalytics.phaedra.base.db/src/eu/openanalytics/phaedra/base/db/prefs/PreferencePage.java
@@ -8,7 +8,6 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 import eu.openanalytics.phaedra.base.db.Activator;
-import eu.openanalytics.phaedra.base.util.misc.VersionUtils;
 
 public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
@@ -25,21 +24,9 @@ public class PreferencePage extends FieldEditorPreferencePage implements IWorkbe
 	protected void createFieldEditors() {
 		Composite parent = getFieldEditorParent();
 		addField(new IntegerFieldEditor(Prefs.DB_TIME_OUT, "Connection timeout:", parent));
-		String phaedraVersion = VersionUtils.getPhaedraVersion();
-		if (phaedraVersion.equals(VersionUtils.UNKNOWN) || phaedraVersion.toLowerCase().contains("dev")) {
-//			Label lbl = new Label(parent, SWT.NONE);
-//			lbl.setText("Advanced Settings:");
-//			Button btn = new Button(parent, SWT.CHECK);
-
-			IntegerFieldEditor dbPoolSize = new IntegerFieldEditor(Prefs.DB_POOL_SIZE, "Database pool size:", parent, 2);
-//			dbPoolSize.setEnabled(false, parent);
-			dbPoolSize.setValidRange(1, 12);
-			addField(dbPoolSize);
-
-//			btn.addListener(SWT.Selection, e -> {
-//				dbPoolSize.setEnabled(btn.getSelection(), parent);
-//			});
-		}
+		IntegerFieldEditor dbPoolSize = new IntegerFieldEditor(Prefs.DB_POOL_SIZE, "Database pool size:", parent, 2);
+		dbPoolSize.setValidRange(1, 12);
+		addField(dbPoolSize);
 	}
 
 	@Override
