b770cf7423f43340774f05713ade03eb8e3fabc3
Frederick Michielssen <fmichielssen@openanalytics.eu>
2019-11-08 17:29:18 +0100

    Added a pattern for custom normalization to refer to a formula

--- eu.openanalytics.phaedra.calculation/src/eu/openanalytics/phaedra/calculation/norm/impl/CustomFormulaNormalizer.java
+++ eu.openanalytics.phaedra.calculation/src/eu/openanalytics/phaedra/calculation/norm/impl/CustomFormulaNormalizer.java
@@ -1,5 +1,8 @@
 package eu.openanalytics.phaedra.calculation.norm.impl;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.script.ScriptException;
 
 import eu.openanalytics.phaedra.base.util.misc.FormulaDescriptor;
@@ -7,17 +10,21 @@ import eu.openanalytics.phaedra.calculation.CalculationException;
 import eu.openanalytics.phaedra.calculation.CalculationService;
 import eu.openanalytics.phaedra.calculation.CalculationService.CalculationLanguage;
 import eu.openanalytics.phaedra.calculation.PlateDataAccessor;
+import eu.openanalytics.phaedra.calculation.formula.FormulaService;
+import eu.openanalytics.phaedra.calculation.formula.model.CalculationFormula;
 import eu.openanalytics.phaedra.calculation.norm.INormalizer;
 import eu.openanalytics.phaedra.calculation.norm.NormalizationException;
 import eu.openanalytics.phaedra.calculation.norm.NormalizationKey;
 import eu.openanalytics.phaedra.calculation.norm.NormalizationService;
 import eu.openanalytics.phaedra.calculation.norm.cache.NormalizedGrid;
+import eu.openanalytics.phaedra.model.plate.util.PlateUtils;
 import eu.openanalytics.phaedra.model.plate.vo.Plate;
 import eu.openanalytics.phaedra.model.plate.vo.Well;
 import eu.openanalytics.phaedra.model.protocol.vo.Feature;
 
 public class CustomFormulaNormalizer implements INormalizer {
 	
+	private static final Pattern FORMULA_REF_PATTERN = Pattern.compile("formula#(\\d+)");
 	
 	@Override
 	public String getId() {
@@ -50,28 +57,39 @@ public class CustomFormulaNormalizer implements INormalizer {
 		
 		PlateDataAccessor accessor = CalculationService.getInstance().getAccessor(plate);
 		String formula = feature.getNormalizationFormula();
-		String language = feature.getNormalizationLanguage();
 		
 		if (formula == null) {
 			throw new NormalizationException("Cannot normalize: feature " + feature + " has no normalization formula");
 		}
 		
-		// Calculate normalized values.
-		for (Well well: plate.getWells()) {
-			Double numericResult = null;
-			try {
-				String result = CalculationLanguage.get(language).eval(formula, accessor, well, feature);
-				numericResult = Double.parseDouble(result);
-			} catch (ScriptException | CalculationException e) {
-				numericResult = Double.NaN;
-			} catch (NumberFormatException e) {
-				numericResult = Double.NaN;
+		Matcher matcher = FORMULA_REF_PATTERN.matcher(formula);
+		if (matcher.matches()) {
+			long formulaId = Long.valueOf(matcher.group(1));
+			CalculationFormula cFormula = FormulaService.getInstance().getFormula(formulaId);
+			if (cFormula == null) throw new NormalizationException("Cannot normalize: formula with ID " + formulaId + " not found.");
+			double[] normValues = FormulaService.getInstance().evaluateFormula(plate, feature, cFormula);
+			for (Well well: plate.getWells()) {
+				int wellNr = PlateUtils.getWellNr(well);
+				grid[well.getRow()-1][well.getColumn()-1] = normValues[wellNr - 1];
+			}
+		} else {
+			// Calculate normalized values.
+			String language = feature.getNormalizationLanguage();
+			for (Well well: plate.getWells()) {
+				Double numericResult = null;
+				try {
+					String result = CalculationLanguage.get(language).eval(formula, accessor, well, feature);
+					numericResult = Double.parseDouble(result);
+				} catch (ScriptException | CalculationException e) {
+					numericResult = Double.NaN;
+				} catch (NumberFormatException e) {
+					numericResult = Double.NaN;
+				}
+				grid[well.getRow()-1][well.getColumn()-1] = numericResult;
 			}
 			
-			grid[well.getRow()-1][well.getColumn()-1] = numericResult;
 		}
 		
 		return new NormalizedGrid(grid);
 	}
-
 }
