cc5ae73d8bbe0fcd5d43945ae0493b477040f212
Frederick Michielssen <fmichielssen@openanalytics.eu>
2019-11-29 16:24:31 +0100

    Added DataCaptureService to script API

--- eu.openanalytics.phaedra.datacapture/src/eu/openanalytics/phaedra/datacapture/scripting/ScriptAPI.java
+++ eu.openanalytics.phaedra.datacapture/src/eu/openanalytics/phaedra/datacapture/scripting/ScriptAPI.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import eu.openanalytics.phaedra.base.imaging.util.ImageIdentifier;
 import eu.openanalytics.phaedra.base.imaging.util.TIFFCodec;
 import eu.openanalytics.phaedra.base.scripting.api.IScriptAPIProvider;
+import eu.openanalytics.phaedra.datacapture.DataCaptureService;
 import eu.openanalytics.phaedra.datacapture.parser.ParserService;
 import eu.openanalytics.phaedra.datacapture.parser.util.ModelUtils;
 import eu.openanalytics.phaedra.datacapture.parser.util.ParserUtils;
@@ -16,6 +17,7 @@ public class ScriptAPI implements IScriptAPIProvider {
 	@Override
 	public Map<String, Object> getServices() {
 		Map<String, Object> utils = new HashMap<>();
+		utils.put("DataCaptureService", DataCaptureService.getInstance());
 		utils.put("ParserService", ParserService.getInstance());
 		utils.put("CaptureUtils", CaptureUtils.class);
 		utils.put("ModelUtils", ModelUtils.class);
